02f2853f56c7ecd148791a403e84ded39d6a6c73
Thomas <satnavthomasp@gmail.com>
2024-09-29 15:18:04 +0100

    Fixed logic error and implemented line inversion

--- Image Manipulation/Invert Image Colours/main.py
+++ Image Manipulation/Invert Image Colours/main.py
@@ -27,7 +27,6 @@ def create_image(input_path, output_path):
         output_image = Image.new('RGB', image.size)
         output_image.putdata(reversed_pixels)
         
-        output_path = output_path
         output_image.save(output_path)
         output_image.show()
 
@@ -42,6 +41,7 @@ def create_checkered_image(input_path, output_path):
         
         reversed_pixels = []
         pixel_num = 0
+        width_is_even = (width % 2 == 0)
         
         for y in range(height):
             for x in range(width):
@@ -50,12 +50,43 @@ def create_checkered_image(input_path, output_path):
                 if pixel_num % 2 == 0:
                     r, g, b = reverse_pixel(r, g, b)
                 reversed_pixels.append((r, g, b))
-            pixel_num += 0
+            
+            if width_is_even:
+                pixel_num += 1
         
         output_image = Image.new('RGB', image.size)
         output_image.putdata(reversed_pixels)
         
-        output_path = output_path
+        output_image.save(output_path)
+        output_image.show()
+
+def create_lined_image(input_path, output_path):
+    file_ext = os.path.splitext(input_path)[1].lower()
+    if file_ext in ['.png', '.jpg', '.jpeg', '.webp']:
+        image = Image.open(input_path)
+        image = image.convert('RGB')
+
+        width, height = image.size
+        pixels = image.load()
+        
+        reversed_pixels = []
+        pixel_num = 0
+        width_is_odd = (width % 2 == 1)
+        
+        for y in range(height):
+            for x in range(width):
+                r, g, b = pixels[x, y]
+                pixel_num += 1
+                if pixel_num % 2 == 0:
+                    r, g, b = reverse_pixel(r, g, b)
+                reversed_pixels.append((r, g, b))
+            
+            if width_is_odd:
+                pixel_num += 1
+        
+        output_image = Image.new('RGB', image.size)
+        output_image.putdata(reversed_pixels)
+        
         output_image.save(output_path)
         output_image.show()
 
@@ -65,11 +96,13 @@ if __name__ == "__main__":
     
     if os.path.isfile(file_path):
         output_path = os.path.join(os.path.dirname(file_path), f"{base_name}_inverted.png")
-        choice = input("'Checkered' or 'Normal': ").lower()
+        choice = input("'Normal', 'Checkered', or 'Lined': ").lower()
         if choice == "normal":
             create_image(file_path, output_path)
         elif choice == "checkered":
             create_checkered_image(file_path, output_path)
+        elif choice == "lined":
+            create_lined_image(file_path, output_path)
         else:
             print("Invalid choice.")
         
